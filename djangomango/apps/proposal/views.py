afd97ab38d5285f5c14f24a58864c48ec1bbc554
Marconi Moreto <caketoad@gmail.com>
2013-01-20 19:06:06 +0800

    Minor tweaks

--- djangomango/apps/proposal/views.py
+++ djangomango/apps/proposal/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.views.generic import CreateView
+from django.utils.translation import ugettext as _
 
 from .forms import SubmitProposalForm
 from .models import PENDING
@@ -7,10 +8,12 @@ from .models import PENDING
 
 class SubmitProposalView(CreateView):
     form_class = SubmitProposalForm
-    template_name = 'mango/submit_proposal.html'
+    template_name = 'proposal/submit_proposal.html'
     success_url = '/'
 
     def form_valid(self, form):
         form.instance.speaker = self.request.user
         form.instance.status = PENDING
+        messages.info(self.request,
+            _("Thank you, your proposal is now being reviewed."))
         return super(SubmitProposalView, self).form_valid(form)
