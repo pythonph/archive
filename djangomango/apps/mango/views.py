5e50a3fec2454522a7a8db6099036b4fd434c914
Marconi Moreto <caketoad@gmail.com>
2013-01-21 11:02:12 +0800

    Switched HomeView to TemplateView and stub context for speakers

--- djangomango/apps/mango/views.py
+++ djangomango/apps/mango/views.py
@@ -1,9 +1,18 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from ..proposal.models import Proposal, APPROVED
 
 
-class HomeView(ListView):
-    template_name = 'mango/approved_proposals.html'
-    queryset = Proposal.objects.filter(status=APPROVED)
-    context_object_name = 'proposal_list'
+class HomeView(TemplateView):
+    template_name = 'mango/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        # add approved proposals
+        context['proposal_list'] = Proposal.objects.filter(status=APPROVED)
+
+        # TODO: add speakers list
+        context['speaker_list'] = None
+
+        return context
