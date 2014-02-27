46248c125eb1027ff241e5855db0ec84912eac31
Mark Steve Samson <hello@marksteve.com>
2014-02-27 09:51:03 +0800

    Initial commit

---
+++ jobs/jobs/settings/base.py
@@ -0,0 +1,49 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env(name):
+    value = os.environ.get(name)
+    if value:
+        return value
+    else:
+        raise ImproperlyConfigured("{} is required.".format(name))
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'jobs.urls'
+
+WSGI_APPLICATION = 'jobs.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
